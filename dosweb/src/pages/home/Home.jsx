51db9359889c4dd96cc13b608f52246819f5f17f
ataibekenov <ataybekenov@gmail.com>
2025-02-10 12:01:28 +0800

    removing testing code

--- dosweb/src/pages/home/Home.jsx
+++ dosweb/src/pages/home/Home.jsx
@@ -10,7 +10,6 @@ import LimitedSales from '../../components/LimitedSales/LimitedSales.jsx';
 
 const Home = () => {
     return(<>
-        <h1>HOME</h1>
         <HeaderCardList></HeaderCardList>
         <NewArrivals></NewArrivals>
         <TopCategories></TopCategories>
